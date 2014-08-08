a0f1451e492b214aa4993ed0794e1087738404b8
Roger Martinez <roger600@gmail.com>
2014-08-08 21:53:16 +0200

    Fixings

--- xpaas-jboss-appserver-docker/conf/scripts/jboss-appserver/startup/0-create-drivers.sh
+++ xpaas-jboss-appserver-docker/conf/scripts/jboss-appserver/startup/0-create-drivers.sh
@@ -4,10 +4,11 @@
 DOCKER_IP=$(/bin/sh /jboss/scripts/docker-ip.sh)
 
 # Configure the MySQL driver, if not already done.
+# Not applicable for domain managed hosts.
 if [ $JBOSS_MODE == "STANDALONE" ]; then
 	echo "Creating mysql driver for standalone mode..."
 	/jboss/scripts/jboss-appserver/jboss-cli.sh -c "/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql)"
-else
+elif [ $JBOSS_MODE == "DOMAIN" ]; then
     echo "Creating mysql driver for domain mode in all default profiles (default, full, full-ha, ha)..."
 	/jboss/scripts/jboss-appserver/jboss-cli.sh -c "/profile=default/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql)"
 	/jboss/scripts/jboss-appserver/jboss-cli.sh -c "/profile=full/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql)"
