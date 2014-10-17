f7f4769f97826bfcfa660c6a420ae901173baed8
nmirasch <neus.miras@gmail.com>
2014-10-17 11:31:48 +0200

    build just images needed for the cluster bpms-eap

--- build-all.sh
+++ build-all.sh
@@ -1,83 +1,49 @@
 #!/bin/sh
-
+set -e
 ##########################################################################################################
 # This script builds all XPaas Docker Images
 # - XPaaS Base
-# - XpaaS Fabric8
 # - XpaaS Zookeeper
 # - XpaaS JBoss Application Server - JBoss EAP
-# - XpaaS JBoss Application Server - JBoss Wildfly
 # - XpaaS HAProxy
 # - XpaaS BPMS for JBoss EAP
-# - XpaaS BPMS for JBoss Wildfly
-# - XpaaS BRMS for JBoss EAP
-# - XpaaS BRMS for JBoss Wildfly
 #
 # NOTE: If necessary, run this script as superuser.
 ##########################################################################################################
 
 pushd .
-echo "Building all XPaaS docker images..."
+echo "Building all XPaaS docker images... eap"
 
 # XPaaS Base
-echo "Buinding XPaaS Base..."
+echo "Building XPaaS Base..."
 cd xpaas-base-docker
 ./build.sh
 cd ..
 
-# XPaaS Fabric8
-echo "Buinding XPaaS Fabric8..."
-cd xpaas-fabric8-docker
-./build.sh
-cd ..
-
 # XPaaS Zookeeper
-echo "Buinding XPaaS Zookeeper..."
+echo "Building XPaaS Zookeeper..."
 cd xpaas-zookeeper-docker
 ./build.sh
 cd ..
 
 # XPaaS JBoss EAP
-echo "Buinding XPaaS JBoss EAP..."
+echo "Building XPaaS JBoss EAP..."
 cd xpaas-jboss-appserver-docker
-./build.sh
-cd ..
-
-# XPaaS JBoss Wildfly
-echo "Buinding XPaaS JBoss Wildfly..."
-cd xpaas-jboss-appserver-docker
-./build.sh wildfly
+./build.sh eap
 cd ..
 
 # XPaaS HAProxy
-echo "Buinding XPaaS HAProxy..."
+echo "Building XPaaS HAProxy..."
 cd xpaas-haproxy-docker
 ./build.sh
 cd ..
 
 # XPaaS BPMS for JBoss EAP
-echo "Buinding XPaaS BPMS for JBoss EAP..."
+echo "Building XPaaS BPMS for JBoss EAP..."
 cd xpaas-bpms-docker/scripts
 ./build.sh bpms-eap
 cd ../..
 
-# XPaaS BPMS for JBoss Wildfly
-echo "Buinding XPaaS BPMS for JBoss Wildfly..."
-cd xpaas-bpms-docker/scripts
-./build.sh bpms-wildfly
-cd ../..
-
-# XPaaS BRMS for JBoss EAP
-echo "Buinding XPaaS BRMS for JBoss EAP..."
-cd xpaas-brms-docker
-./build.sh brms-eap
-cd ..
-
-# XPaaS BRMS for JBoss Wildfly
-echo "Buinding XPaaS BRMS for JBoss Wildfly..."
-cd xpaas-brms-docker
-./build.sh brms-wildfly
-cd ..
 
 echo "Build completed."
 popd
